f24c782d1c3fca2df58a32de43d17f0e3c4ab69d
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-02-20 16:17:25 -0500

    Time: 304 ms (14.89%), Space: 17.8 MB (64.24%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -17,7 +17,7 @@ class Solution:
         def sumLeaves(root, currH, maxH, total):
             if not root:
                 return 0
-            if root and currH == maxH-1:
+            elif currH == maxH-1:
                 return root.val
             total = sumLeaves(root.left, currH+1, maxH, total)
             total += sumLeaves(root.right, currH+1, maxH, total)
