eee915c0c5a1e967a1c20825134c6a1a0b3a3c8e
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-02-20 16:16:35 -0500

    Time: 303 ms (15.08%), Space: 17.7 MB (93.59%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -5,27 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def findDepth(self, maxDepth, currDepth, root):
-        if root == None:
-            if currDepth > maxDepth:
-                maxDepth = currDepth
-            return maxDepth
-        maxDepth = self.findDepth(maxDepth, currDepth + 1, root.left)
-        maxDepth = self.findDepth(maxDepth, currDepth + 1, root.right) 
-        return maxDepth
-
-    def findSum(self, maxDepth, maxSum, currDepth, root):
-        if root == None:
-            return maxSum
-        if currDepth == maxDepth:       
-            maxSum += root.val
-        maxSum = self.findSum(maxDepth, maxSum, currDepth + 1, root.left)
-        maxSum = self.findSum(maxDepth, maxSum, currDepth + 1, root.right) 
-        return maxSum 
-         
-
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        maxDepth = self.findDepth(0, 0, root)
-        maxSum = self.findSum(maxDepth, 0, 1, root)
-        return maxSum
+        def maxHeight(root, maxH, currH):
+            if not root:
+                maxH = max(maxH, currH)
+                return maxH
+            lHeight = maxHeight(root.left, maxH, currH+1)
+            rHeight = maxHeight(root.right, maxH, currH+1)
+            return max(lHeight, rHeight)
+        
+        def sumLeaves(root, currH, maxH, total):
+            if not root:
+                return 0
+            if root and currH == maxH-1:
+                return root.val
+            total = sumLeaves(root.left, currH+1, maxH, total)
+            total += sumLeaves(root.right, currH+1, maxH, total)
+            return total
+        
+        maxH = maxHeight(root, 0, 0)
+        return sumLeaves(root, 0, maxH, 0)
+            
+        
