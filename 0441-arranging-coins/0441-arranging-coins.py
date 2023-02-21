4c79106e074efc1058b451e95967dfcd75c68f11
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-02-21 18:10:48 -0500

    Time: 1142 ms (17.25%), Space: 13.8 MB (94.27%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -2,11 +2,9 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         flag = True
         i = 1
-        # counter = 0
         while flag:
             if n - i < 0:
                 return i-1
             n -= i
             i += 1
-            # counter += 1
         
