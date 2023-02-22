41cae55340104e5c5877f30527b4220e4ff450e7
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-02-21 19:41:45 -0500

    Time: 495 ms (21.73%), Space: 14.9 MB (98.89%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -0,0 +1,30 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        kArr = [int(s) for s in str(k)]
+        i, j = len(num)-1, len(kArr)-1
+        carry = 0
+        # idx = len(num) - 1
+        while i >= 0 and j >= 0:
+            s = num[i] + kArr[j] + carry
+            num[i] = s % 10
+            carry = s // 10
+            i -= 1
+            j -= 1
+        # print("num =", num)
+        while i >= 0:
+            s = num[i] + carry 
+            num[i] = s % 10
+            carry = s // 10
+            i -= 1
+        # print("num =", num)
+        while j >= 0:
+            s = kArr[j] + carry 
+            num.insert(0, s % 10)
+            carry = s // 10
+            j -= 1
+        # print("num =", num)
+        if carry > 0:
+            num.insert(0, carry)
+        return num
+            
+        
