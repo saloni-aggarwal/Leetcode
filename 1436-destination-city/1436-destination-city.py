810b5779c43a636b16c1e5485835654a52a1dac9
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-02-23 20:23:19 -0500

    Time: 61 ms (35.78%), Space: 13.9 MB (75.16%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -4,10 +4,7 @@ class Solution:
         cityMap = defaultdict(list)
         for path in paths:
             cityMap[path[0]].append(path[1])
-        # print("cityMap =", cityMap)
         for path in paths:
-            # print("city =", city)
-            # print("cityMap[city]", cityMap[city])
             if path[1] not in cityMap:
                 return path[1]
         return ""
