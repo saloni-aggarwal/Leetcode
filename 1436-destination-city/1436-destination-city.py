5942e3afe0fdc3f0b3d1b73b65704d2f2d541304
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-02-23 20:25:06 -0500

    Time: 58 ms (50.88%), Space: 13.9 MB (75.16%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,9 +1,9 @@
 # from collections import defaultdict
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        src = []
+        src = set()
         for path in paths:
-            src.append(path[0])
+            src.add(path[0])
         for path in paths:
             if path[1] not in src:
                 return path[1]
