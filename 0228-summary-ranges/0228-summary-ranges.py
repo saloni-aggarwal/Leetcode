0a20444eb3541be8a7507c972b941e9155d4aac4
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-02 17:40:11 -0500

    Time: 35 ms (42.14%), Space: 13.8 MB (60.35%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,28 +2,24 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if len(nums) == 0:
             return []
-        start = i = nums[0]
-        # st = ""
+        start = nums[0]
         ans = []
         for j in range(len(nums)):
-            # print("i =", i, "j =", j, "nums[j] =", nums[j])
-            # print("start =", start)
-            if i != nums[j]:
-                # print("in if")
-                if start != nums[j-1]:
-                    st = str(start) + "->" + str(nums[j-1])
+            if j+1 == len(nums) or nums[j]+1 != nums[j+1]:
+                if start != nums[j]:
+                    ans.append(str(start) + "->" + str(nums[j]))
                 else:
-                    st = str(start)
-                ans.append(st)
-                if j != len(nums):
-                    start = i = nums[j]
-            # else:
-                # print("in else")
-            i += 1
-        if start != nums[-1]:
-            st = str(start) + "->" + str(nums[-1])
-        else:
-            st = str(start)
-        ans.append(st)
+                    ans.append(str(start))
+                if j < len(nums)-1:
+                    start = nums[j+1]
+                # ans.append(st)
+                # if j != len(nums):
+                #     start = nums[j+1]
+            # i += 1
+        # if start != nums[-1]:
+        #     st = str(start) + "->" + str(nums[-1])
+        # else:
+        #     st = str(start)
+        # ans.append(st)
         return ans
             
