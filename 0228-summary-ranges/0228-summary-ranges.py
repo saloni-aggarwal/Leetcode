d1f481d038382b26e357ba4b2c221afde467b25f
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-02 17:40:26 -0500

    Time: 31 ms (69.41%), Space: 13.8 MB (60.35%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -12,14 +12,5 @@ class Solution:
                     ans.append(str(start))
                 if j < len(nums)-1:
                     start = nums[j+1]
-                # ans.append(st)
-                # if j != len(nums):
-                #     start = nums[j+1]
-            # i += 1
-        # if start != nums[-1]:
-        #     st = str(start) + "->" + str(nums[-1])
-        # else:
-        #     st = str(start)
-        # ans.append(st)
         return ans
             
