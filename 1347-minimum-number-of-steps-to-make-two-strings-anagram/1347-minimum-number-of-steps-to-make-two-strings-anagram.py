a214b76db8a118d690a6903564ca26e5bc2ac06c
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-03 19:14:56 -0500

    Time: 112 ms (89.73%), Space: 14.5 MB (98.45%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -2,11 +2,8 @@ class Solution:
     def minSteps(self, s: str, t: str) -> int:
         countS = Counter(s)
         countT = Counter(t)
-        # print("countS =", countS)
-        # print("countT =", countT)
         total = 0
         for ch in countT:
-            # print("ch =", ch, "total =", total)
             if ch not in countS:
                 total += countT[ch]
             else:
