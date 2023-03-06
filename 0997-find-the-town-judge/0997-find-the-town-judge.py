5a979b86e642011714080842053c0d1432916486
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-05 19:14:51 -0500

    Time: 750 ms (81.80%), Space: 18.8 MB (91.05%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -2,13 +2,13 @@ from collections import defaultdict
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         trustOn = [0] * n
-        trustTo = [0] * n
+        trustFrom = [0] * n
         
         for a, b in trust:
             trustOn[b-1] += 1
-            trustTo[a-1] += 1
+            trustFrom[a-1] += 1
         for i in range(n):
-            if trustOn[i] == n-1 and trustTo[i] == 0:
+            if trustOn[i] == n-1 and trustFrom[i] == 0:
                 return i+1
         return -1
             
