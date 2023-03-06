547d1e8e51d0a0fc06a4806cf672048c23c539fe
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-06 18:30:25 -0500

    Time: 26 ms (94.19%), Space: 13.8 MB (56.84%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -8,21 +8,21 @@ class Solution:
         return False
             
     def equalFrequency(self, word: str) -> bool:
-        chFreq = Counter(word)
+#         chFreq = Counter(word)
         
-#         If there are only one kind of characters in word 
-#         eg: "zzzz"
-        if len(chFreq) == 1:
-            return True
+# #         If there are only one kind of characters in word 
+# #         eg: "zzzz"
+#         if len(chFreq) == 1:
+#             return True
         
-#         To check if all characters have same count
-#         If the count is 1 then return True else False
-        uniqueFreq = set(chFreq.values())
-        if len(uniqueFreq) == 1:
-            if uniqueFreq.pop() == 1:
-                return True
-            else:
-                return False
+# #         To check if all characters have same count
+# #         If the count is 1 then return True else False
+#         uniqueFreq = set(chFreq.values())
+#         if len(uniqueFreq) == 1:
+#             if uniqueFreq.pop() == 1:
+#                 return True
+#             else:
+#                 return False
         
 #         For every character in the word remove each character
 #         Then check if the remaining word satisfies the given condition
