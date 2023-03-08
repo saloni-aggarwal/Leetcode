40fa0819082a03c501ec0fadd8b95f6ae2ed3a3e
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-07 19:17:48 -0500

    Time: 40 ms (50.85%), Space: 14 MB (24.75%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,14 +1,13 @@
-from collections import defaultdict
 import string
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         paragraph = paragraph.lower().replace(',', ' ')
         paragraph = paragraph.translate(str.maketrans('','',string.punctuation))
+        
         wordArr = paragraph.split()
+        
         wordCounter = Counter(wordArr)
         
-        # for word in wordArr:
-        #     wordCounter[word] += 1
         maxCnt = float('-inf')
         maxWord = ""
         
