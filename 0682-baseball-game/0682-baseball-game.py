744a8eb502f9844084668fd12604914c368aca68
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-08 19:57:16 -0500

    Time: 45 ms (45.72%), Space: 14.1 MB (25.70%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,24 +1,20 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         score = []
-        # print(operations[2].isdigit())
         for operation in operations:
-            print("score =", score)
-            # if operation.isdigit():
-            #     score.append(int(operation))
             if operation == 'D':
-                prev = score.pop()
-                score.append(prev)
-                score.append(2*prev)
+                # prev = score.pop()
+                # score.append(prev)
+                score.append(2*score[-1])
             elif operation == 'C':
                 score.pop()
             elif operation == '+':
                 if len(score) >= 2:
-                    first = score.pop()
-                    second = score.pop()
-                    score.append(second)
-                    score.append(first)
-                    score.append(first + second)
+                    # first = score.pop()
+                    # second = score.pop()
+                    # score.append(second)
+                    # score.append(first)
+                    score.append(score[-1] + score[-2])
             else:
                 score.append(int(operation))
         return sum(score)
