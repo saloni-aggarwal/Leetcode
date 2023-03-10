b5c82e75bc895f7809dcfa58475857641ae3c5ef
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-10 17:32:41 -0500

    Time: 35 ms (38.84%), Space: 13.8 MB (55.99%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -3,23 +3,21 @@ class Solution:
         first = set("qwertyuiop")
         second = set("asdfghjkl")
         third = set("zxcvbnm")
-        # print("first =", first, "second =", second, "third =", third)
+        
         ans = []
         for word in words:
             w = set(word.lower())
             f = s = t = True
-            # print("w =", w)
             for ch in w:
-                # print("ch =", ch)
                 if ch not in first:
                     f = False
                 if ch not in second:
                     s = False
                 if ch not in third:
                     t = False
-            # print("f =", f, "s =", s, "t =", t)
             if f or s or t:
                 ans.append(word)
+                
         return ans
             
         
