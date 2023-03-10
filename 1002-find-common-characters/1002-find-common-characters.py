f41ceec13b1f78c91da58ed9992fe20ff6d280e3
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-03-09 19:57:15 -0500

    Time: 46 ms (86.03%), Space: 14 MB (55.05%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -5,23 +5,17 @@ class Solution:
         if len(words) == 1:
             return s
         common = Counter(s)
-        # for ch in words[0]:
-        #     if ch in words[1]:
-        #         common[ch] += 1
-        # print(common)
+        
         for i in range(1, len(words)):
-            # print("words[i] =", words[i])
-            # print("common =", common)
             letters = [x for x in words[i]]
-            # print(letters)
             letters = Counter(letters)
-            # print(letters)
             for ch in common:
                 if common[ch] != 0:
                     if ch not in letters:
                         common[ch] == 0
                     if letters[ch] < common[ch]:
                         common[ch] = letters[ch]
+                        
         ans = []        
         for ch in common:
             if common[ch] != 0:
