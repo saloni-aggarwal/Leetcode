7b3636e371324225dae321fc1fc1e04f7af75622
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-08 13:54:12 -0400

    Time: 8 ms (51.92%), Space: 42.9 MB (47.81%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
         Map<Integer, List<Integer>> cntList = new HashMap<>();
+        List<Integer> temp;
+        
         for(int val: arr) {
             int cnt = Integer.bitCount(val);
-            List<Integer> temp = cntList.getOrDefault(cnt, new ArrayList<>());
+            temp = cntList.getOrDefault(cnt, new ArrayList<>());
             temp.add(val);
             cntList.put(cnt, temp);
         }
+        
         int[] ans = new int[arr.length];
         int idx = 0;
         for(Map.Entry<Integer, List<Integer>> pair : cntList.entrySet()) {
-            List<Integer> temp = pair.getValue();
+            temp = pair.getValue();
             Collections.sort(temp);
             for(int val: temp) {
                 ans[idx] = val;
