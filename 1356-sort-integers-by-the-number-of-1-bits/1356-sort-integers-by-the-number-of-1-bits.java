207658e91c2c5a1f6d5803e53dc137b13ace4311
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-08 14:02:18 -0400

    Time: 14 ms (21.60%), Space: 43 MB (37.87%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,26 +1,37 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        Map<Integer, List<Integer>> cntList = new HashMap<>();
-        List<Integer> temp;
+//         Map<Integer, List<Integer>> cntList = new HashMap<>();
+//         List<Integer> temp;
         
-        for(int val: arr) {
-            int cnt = Integer.bitCount(val);
-            temp = cntList.getOrDefault(cnt, new ArrayList<>());
-            temp.add(val);
-            cntList.put(cnt, temp);
-        }
+//         for(int val: arr) {
+//             int cnt = Integer.bitCount(val);
+//             temp = cntList.getOrDefault(cnt, new ArrayList<>());
+//             temp.add(val);
+//             cntList.put(cnt, temp);
+//         }
         
         int[] ans = new int[arr.length];
-        int idx = 0;
-        for(Map.Entry<Integer, List<Integer>> pair : cntList.entrySet()) {
-            temp = pair.getValue();
-            Collections.sort(temp);
-            for(int val: temp) {
-                ans[idx] = val;
-                idx += 1;
-            }
+//         int idx = 0;
+//         for(Map.Entry<Integer, List<Integer>> pair : cntList.entrySet()) {
+//             temp = pair.getValue();
+//             Collections.sort(temp);
+//             for(int val: temp) {
+//                 ans[idx] = val;
+//                 idx += 1;
+//             }
                 
-        }
-        return ans;
+//         }
+        var nums = Arrays.stream(arr)
+					 .boxed()
+					 .toArray(Integer[]::new);
+        // System.out.println(Arrays.toString(nums));
+        
+        Arrays.sort(nums, Comparator.comparingInt(Integer::bitCount)
+							    .thenComparingInt(n -> n));
+        // System.out.println(Arrays.toString(nums));
+        
+        return Arrays.stream(nums)
+				 .mapToInt(Integer::intValue)
+				 .toArray();
     }
 }
