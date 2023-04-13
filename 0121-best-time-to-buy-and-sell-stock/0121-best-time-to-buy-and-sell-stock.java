fe06f6d1afa9e0a91d97ab7c897cb2e9756089a2
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-12 23:03:46 -0400

    Time: 1 ms (99.86%), Space: 58.6 MB (92.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] maxTrack = new int[prices.length];
+//         Approach 1
+        /* int[] maxTrack = new int[prices.length];
         maxTrack[prices.length-1] = prices[prices.length-1];
         for(int i = prices.length-2; i >= 0; i--) {
             maxTrack[i] = Math.max(maxTrack[i+1], prices[i]);
@@ -9,6 +10,15 @@ class Solution {
         for(int i = 0; i < prices.length; i++) {
             ans = Math.max(ans, maxTrack[i]-prices[i]);
         }
-        return ans;
+        return ans; */
+        
+//         Approach 2
+        int leftVal = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int price: prices) {
+            leftVal = Math.min(leftVal, price);
+            profit = Math.max(profit, price-leftVal);
+        }
+        return profit;
     }
 }
