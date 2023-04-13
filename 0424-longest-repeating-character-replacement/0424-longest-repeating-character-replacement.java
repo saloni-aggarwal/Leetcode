85e023dfbf77c2b24974f33abb3212ca2b37015a
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-13 19:29:04 -0400

    Time: 297 ms (5.02%), Space: 43.6 MB (5.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,12 +2,14 @@ class Solution {
     public int characterReplacement(String s, int k) {
         int[] arr = new int[26];
         int beg = 0, maxLen = 0, largestCnt = 0;
+        
         for(int end = 0; end < s.length(); end++) {
             arr[s.charAt(end) - 'A']++;
             largestCnt = Math.max(largestCnt, arr[s.charAt(end) - 'A']);
             
             if(end - beg + 1 - largestCnt > k) {
-                arr[s.charAt(beg++) - 'A'] --;                
+                arr[s.charAt(beg++) - 'A'] --; 
+                System.out.println(Arrays.toString(arr));
             }
             
             maxLen = Math.max(maxLen, end-beg+1);
