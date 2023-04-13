a8e29a769567ed3b15acd531a685348f35d3bf8a
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-12 21:56:09 -0400

    Time: 2 ms (81.89%), Space: 42.9 MB (37.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, List<Integer>> idx = new HashMap<>();
+        // Method 1
+        
+/*         Map<Integer, List<Integer>> idx = new HashMap<>();
         Map<Integer, Integer> count = new HashMap<>();
         List<Integer> temp;
         for(int i = 0; i < nums.length; i++) {
@@ -16,6 +18,16 @@ class Solution {
                 return new int[]{idx.get(num).get(0), idx.get(num).get(1)};
             } else if(remaining != num && idx.containsKey(remaining)) {
                 return new int[]{idx.get(num).get(0), idx.get(remaining).get(0)};
+            }
+        } */
+        
+        Map<Integer, Integer> idx = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(idx.containsKey(nums[i])) {
+                int val = idx.get(nums[i]);
+                return new int[]{val, i};
+            } else {
+                idx.put(target-nums[i], i);
             }
         }
         
