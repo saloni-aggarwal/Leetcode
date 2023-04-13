07737faee6ae3de226ebd693f8e7d3dd62ff5356
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-12 21:40:54 -0400

    Time: 12 ms (54.49%), Space: 43 MB (28.42%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, List<Integer>> idx = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer> temp;
+        for(int i = 0; i < nums.length; i++) {
+            temp = idx.getOrDefault(nums[i], new ArrayList<>());
+            temp.add(i);
+            idx.put(nums[i], temp);
+            count.put(nums[i], count.getOrDefault(nums[i], 0)+1);
+        }
+        // System.out.println(idx);
+        for(int num: nums) {
+            int remaining = target - num;
+            if(remaining == num && count.get(num) > 1) {
+                return new int[]{idx.get(num).get(0), idx.get(num).get(1)};
+            } else if(remaining != num && idx.containsKey(remaining)) {
+                return new int[]{idx.get(num).get(0), idx.get(remaining).get(0)};
+            }
+        }
+        return new int[2];
+    }
+}
