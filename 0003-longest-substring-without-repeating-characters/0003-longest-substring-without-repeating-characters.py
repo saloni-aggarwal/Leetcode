78f6804ba628202435a82d771fd4ce1b006455fc
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-13 16:39:26 -0400

    Time: 57 ms (84.98%), Space: 14 MB (86.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,14 +3,14 @@ class Solution:
         ans = 0
         lst = []
         for i in range(len(s)):
-            if s[i] not in lst:
-                lst.append(s[i])
-                if i == len(s)-1:
-                    return max(ans, len(lst))                
-            else:
+            if s[i] in lst:
+            #     lst.append(s[i])
+            #     if i == len(s)-1:
+            #         return max(ans, len(lst))                
+            # else:
                 ans = max(ans, len(lst))
                 ind = lst.index(s[i])
                 lst = lst[ind+1:]
-                lst.append(s[i])
-        return ans
+            lst.append(s[i])
+        return max(ans, len(lst))
         
