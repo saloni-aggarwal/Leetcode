a0c6961e38f30f5cb1de45c5976a1d6ab1e1fc4b
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-13 16:00:33 -0400

    Time: 10 ms (33.26%), Space: 47.6 MB (27.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        
         int[][] res = new int[intervals.length][2];
-        int idx = 0;
-        int idx2 = 0;
+        int idx = 0, idx2 = 0;
+        
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        
         while(idx < intervals.length) {
             int start = intervals[idx][0];
             int end = intervals[idx][1];
             while(idx < intervals.length && intervals[idx][0] <= end) {
                 end = Math.max(end, intervals[idx++][1]);
             }
-            // System.out.println("end =" +end);
             res[idx2++] = new int[]{start, end};
         }
-        // System.out.println("idx2=" +idx2);
+        
         return Arrays.copyOfRange(res, 0, idx2);
         
     }
