60a11a2c4d899e32247179c5d3d546368ae77f66
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-14 19:56:39 -0400

    Time: 961 ms (11.88%), Space: 43.1 MB (19.18%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // System.out.println("s1 = " + s1 + " s2 = " + s2);
+        
         char[] temp;
+        
         temp = s1.toCharArray();
         Arrays.sort(temp);
         s1 = new String(temp);
+        
         int len = s1.length();
+        
         for(int i = 0; i <= s2.length()-len; i++) {
             temp = s2.substring(i, i+s1.length()).toCharArray();
-            // System.out.println(Arrays.toString(temp));
             Arrays.sort(temp);
             String compare = new String(temp);
-            // System.out.println("s1 = " + s1 + " compare = " + compare);
+            
             if(compare.equals(s1)) {
                 return true;
             }
         }
+        
         return false;
     }
 }
