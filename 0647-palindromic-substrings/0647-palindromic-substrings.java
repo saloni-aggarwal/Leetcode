678ca25b269b2e4ffb77b06e6705f4c8cb0c0b84
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-17 09:43:24 -0400

    Time: 4 ms (72.17%), Space: 40.6 MB (70.05%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,43 @@
 class Solution {
-    public boolean isPalindrome(String s, int i, int j) {
-        while(i < j) {
-            if(s.charAt(i++) != s.charAt(j--))
-                return false;
-        }
-        return true;
-    }
-    public int countSubstrings(String s) {
+//     Method 1
+//     public boolean isPalindrome(String s, int i, int j) {
+//         while(i < j) {
+//             if(s.charAt(i++) != s.charAt(j--))
+//                 return false;
+//         }
+//         return true;
+//     }
+//     public int countSubstrings(String s) {
+//         int ans = 0;
+        
+//         for(int i = 0; i < s.length(); i++) {
+//             for(int j = i; j < s.length(); j++) {
+//                 if(isPalindrome(s, i, j)) {
+//                     ans += 1;
+//                 }
+//             }
+//         }
+//         return ans;
+        
+//     }
+    public int countSubStr(String s, int i, int j) {
         int ans = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            for(int j = i; j < s.length(); j++) {
-                if(isPalindrome(s, i, j)) {
-                    ans += 1;
-                }
-            }
+        while(i >= 0 && j < s.length()) {
+            if(s.charAt(i--) != s.charAt(j++))
+                break;
+            ans += 1;
         }
         return ans;
-        
     }
+    
+     public int countSubstrings(String s) {
+         int ans = 0;
+         
+         for(int i = 0; i < s.length(); i++) {
+             ans += countSubStr(s, i, i);
+             ans += countSubStr(s, i, i+1);
+         }
+         return ans;
+     }
 }
