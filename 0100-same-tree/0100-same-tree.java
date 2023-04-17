f0e65d12f4b88c579dd70e53ff1123ddb978399d
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-16 20:38:58 -0400

    Time: 0 ms (100.00%), Space: 40.1 MB (49.73%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,15 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null)
+        if(p == null && q == null) 
             return true;
-        if (p == null || q == null || p.val != q.val)
+        else if(p == null || q == null) 
             return false;
-        // if (!isSameTree(p.left, q.left))
-        //     return false;
-        // if (!isSameTree(p.right, q.right))
-        //     return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        else if(p.val != q.val)
+            return false;
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
         
     }
 }
