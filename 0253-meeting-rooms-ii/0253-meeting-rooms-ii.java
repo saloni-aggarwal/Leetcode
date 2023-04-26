b3051d299e111cd8254377c81940f58c3d9305b9
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-04-26 19:03:34 -0400

    Time: 9 ms (29.51%), Space: 43 MB (25.50%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals.length == 0)
+            return 0;
+        
+        Arrays.sort(intervals, new Comparator<>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        
+        PriorityQueue<Integer> track = new PriorityQueue<>();
+        track.add(intervals[0][1]);
+        
+        for(int i = 1; i < intervals.length; i++) {
+            // System.out.println(track);
+            if(track.peek() <= intervals[i][0])
+                track.poll();
+            track.add(intervals[i][1]);
+            
+        }
+        // System.out.println(track);
+        return track.size();
+    }
+}
