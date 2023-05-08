d5d334c1be85ba480ac55dc11862713cf2a7411e
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-05-07 21:02:52 -0400

    Time: 13 ms (29.23%), Space: 60.3 MB (5.16%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,59 @@
+class Solution {
+    public void dfs(int[][] grid, int i, int j, int m, int n) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0)
+            return;
+        
+        grid[i][j] = 0;
+        
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        
+        for(int[] dir: dirs) {
+            dfs(grid, i+dir[0], j+dir[1], m, n);
+        }
+    }
+    
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i = 0; i < m; i++) {
+            if(grid[i][0] == 1) {
+                dfs(grid, i, 0, m, n);
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            if(grid[i][n-1] == 1) {
+                dfs(grid, i, n-1, m, n);
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(grid[0][i] == 1) {
+                dfs(grid, 0, i, m, n);
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(grid[m-1][i] == 1) {
+                dfs(grid, m-1, i, m, n);
+            }
+        }
+        
+        // for(int i = 0; i < m; i++) {
+        //     System.out.println(Arrays.toString(grid[i]));
+        // }
+        
+        int count = 0;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
