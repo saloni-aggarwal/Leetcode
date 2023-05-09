2fc39d859e7ca26567dae8971db97b12cbff6554
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-05-08 21:47:11 -0400

    Time: 1 ms (30.65%), Space: 42.1 MB (97.04%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -4,20 +4,12 @@ class Solution {
         int n = mat.length;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(i == j) {
-                    // System.out.println("first if mat[i][j] = " + mat[i][j]);
+                if(i == j) 
                     total += mat[i][j];
-                } 
-                else if((i + j) == n-1) {
-                    // System.out.println("second if mat[i][j] = " + mat[i][j]);
+                else if((i + j) == n-1) 
                     total += mat[i][j];
-                }
-                // System.out.println("total = " + total);
             }
         }
-        // if(n%2 != 0 && n != 1) {
-        //     total -= mat[n/2][n/2];
-        // }
         return total;
         
     }
