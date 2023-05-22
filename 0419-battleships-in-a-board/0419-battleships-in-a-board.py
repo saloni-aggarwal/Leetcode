d8696818edf45094105073e28eb75715d80e62f3
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-05-22 19:10:51 -0400

    Time: 58 ms (46.15%), Space: 16.9 MB (46.15%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -5,10 +5,10 @@ class Solution(object):
         :rtype: int
         """
         battleships = 0
-        n, m = len(board), len(board[0])
+        # n, m = , 
         
-        for i in range(n):
-            for j in range(m):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
                 left = board[i][j-1] if j-1 >= 0 else '.'
                 top = board[i-1][j] if i-1 >= 0 else '.'
                 if board[i][j] == 'X' and left != 'X' and top != 'X':
