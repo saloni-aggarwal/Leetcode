847260778678a9052497c3c9bf0fadeaeaec68f6
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-05-30 01:30:36 +0530

    Time: 62 ms (75.12%), Space: 16.4 MB (8.65%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -2,19 +2,40 @@ from collections import defaultdict
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        count = defaultdict(int)
+#         Method 1
+#         count = defaultdict(int)
         
-        for num in nums:
-            count[num] += 1
+#         for num in nums:
+#             count[num] += 1
             
-        k = 0
+#         k = 0
         
-        for c in count:
-            t = min(2, count.get(c))
-            for _ in range(t):
-                nums[k] = c
-                k += 1
+#         for c in count:
+#             t = min(2, count.get(c))
+#             for _ in range(t):
+#                 nums[k] = c
+#                 k += 1
             
-        return k
+#         return k
         
+        
+#         Method 2
+
+        i, count = 1, 1
+    
+        while i < len(nums):
+            
+            if nums[i] == nums[i-1]:
+                count += 1
+                
+                if count > 2:
+                    nums.pop(i)
+                    i -= 1
+                    
+            else:
+                count = 1
+            
+            i += 1
+            
+        return i
         
