3cc6a4735d6681defc661110cd0075ab0d5801fe
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-06-08 18:57:37 -0400

    Time: 48 ms (27.86%), Space: 16.2 MB (48.21%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,9 +1,26 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        wordList = s.split()
-        words = [""] * len(wordList)
-        for word in wordList:
-            idx = int(word[-1])
-            words[idx-1] = word[:len(word)-1]
+        
+#         APPROACH 1
+        # wordList = s.split()
+        # words = [""] * len(wordList)
+        # for word in wordList:
+        #     idx = int(word[-1])
+        #     words[idx-1] = word[:len(word)-1]
+        
+#         APPROACH 2
+    
+        words = [""] * len(s.split())
+        word = ""
+        for i in range(len(s)):
+            if s[i] == " ":
+                continue
+            if s[i].isdigit():
+                words[int(s[i])-1] = word
+                i += 1
+                word = ""
+            else:
+                word += s[i]
+            
             
         return ' '.join(words)
