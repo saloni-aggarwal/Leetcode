f35e1b174c56d5decd0518b2f65d382c80751e8f
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-06-21 19:41:38 -0400

    Time: 155 ms (51.76%), Space: 19.6 MB (46.23%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,18 +1,15 @@
 class Solution:
-    
-    
-    def totalArea(self, grid, x, y, m, n, total, dirs):
+    def totalArea(self, grid, x, y, m, n, total):
         if grid[x][y] == 0:
-            return 0
-        
+            return 0       
         grid[x][y] = 0
         total += 1
-        
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
         
         for dirX, dirY in dirs:
             currX, currY = x+dirX, y+dirY
             if 0 <= currX < m and 0 <= currY < n:
-                total = max(total, self.totalArea(grid, currX, currY, m, n, total, dirs))
+                total = max(total, self.totalArea(grid, currX, currY, m, n, total))
                 
         return total
     
@@ -22,12 +19,11 @@ class Solution:
         ans = 0
         m = len(grid)
         n = len(grid[0])
-        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
         
         for x in range(m):
             for y in range(n):
                 if grid[x][y] == 1:
-                    ans = max(ans, self.totalArea(grid, x, y, m, n, 0, dirs))
+                    ans = max(ans, self.totalArea(grid, x, y, m, n, 0))
         
         return ans
          
