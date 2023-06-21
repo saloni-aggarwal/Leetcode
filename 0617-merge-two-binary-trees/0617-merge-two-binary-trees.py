5e61a6e0b69632d33944494bd11e0e8cea5e5f9b
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-06-21 17:15:40 -0400

    Time: 100 ms (45.26%), Space: 17.9 MB (36.11%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -7,27 +7,34 @@
 class Solution:
         
     def helper(self, root1, root2):
-        if not root1 and not root2:
-            return None
-        val = 0
-        if root1:
-            val += root1.val
-        if root2:
-            val += root2.val
+        # if not root1 and not root2:
+        #     return None
+        # val = 0
+        # if root1:
+        #     val += root1.val
+        # if root2:
+        #     val += root2.val
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        root1.val += root2.val
+        root1.left = self.helper(root1.left, root2.left)
+        root1.right = self.helper(root1.right, root2.right)
             
-        root = TreeNode(val)
+        # root = TreeNode(val)
         
-        if root1 and root2:
-            root.left = self.helper(root1.left, root2.left)
-            root.right = self.helper(root1.right, root2.right)
-        elif root1:
-            root.left = self.helper(root1.left, None)
-            root.right = self.helper(root1.right, None)
-        elif root2:
-            root.left = self.helper(None, root2.left)
-            root.right = self.helper(None, root2.right)
+        # if root1 and root2:
+        #     root.left = self.helper(root1.left, root2.left)
+        #     root.right = self.helper(root1.right, root2.right)
+        # elif root1:
+        #     root.left = self.helper(root1.left, None)
+        #     root.right = self.helper(root1.right, None)
+        # elif root2:
+        #     root.left = self.helper(None, root2.left)
+        #     root.right = self.helper(None, root2.right)
             
-        return root
+        return root1
         
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         return self.helper(root1, root2)
