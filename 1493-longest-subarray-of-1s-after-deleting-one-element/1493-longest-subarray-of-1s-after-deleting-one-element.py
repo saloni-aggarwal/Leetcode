3d59d7e2926796ef327594d17a8a6d2834cf9e11
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-07-04 21:45:02 -0400

    Time: 2127 ms (5.03%), Space: 20 MB (11.96%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countOnes(self, nums, n, longest):
+        i = 0
+        while i < n:
+            temp = 0
+            while i < n and nums[i] == 1:
+                temp += 1
+                i += 1
+            longest = max(longest, temp)
+            i += 1
+        return longest
+                
+    def longestSubarray(self, nums: List[int]) -> int:        
+        n = len(nums)
+        longest = self.countOnes(nums, n, 0)  
+        if longest == n:
+            return n-1
+        for i in range(n):
+            if nums[i] == 0:
+                temp = nums[:i] + nums[i+1:]
+                longest = max(longest, self.countOnes(temp, n-1, longest))
+        return longest
