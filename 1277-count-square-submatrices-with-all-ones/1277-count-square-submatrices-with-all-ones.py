76eb94bffb44b77899a8c85edc4a95b7cbeaf49e
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-07-05 22:47:26 -0400

    Time: 637 ms (27.85%), Space: 18.6 MB (89.47%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -4,10 +4,4 @@ class Solution:
             for j in range(1, len(matrix[0])):
                 matrix[i][j] *= min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]) + 1
         
-#         total = 0
-#         for i in range(len(matrix)):
-#             total += sum(matrix[i])
-        
-#         # print()
-        
         return sum(map(sum, matrix))
