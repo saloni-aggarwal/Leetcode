82ac6f7a29385d3eba7cdd953f39f3d9c32fcc5b
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-08-07 15:19:55 -0400

    Time: 42 ms (85.65%), Space: 16.3 MB (74.89%) - LeetHub

---
+++ 0760-find-anagram-mappings/0760-find-anagram-mappings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        counter = collections.Counter(nums2)
+        index = collections.defaultdict(list)
+        # print(counter)
+        
+        for i, num in enumerate(nums2):
+            index[num].append(i)
+            
+        res = []
+        
+        for num in nums1:
+            res.append(index[num][counter[num]-1])
+            counter[num] -= 1
+        return res
+            
