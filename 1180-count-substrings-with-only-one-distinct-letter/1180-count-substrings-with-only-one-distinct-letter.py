72496a10c7f6fc0b177a64a705df3750a6d50dd0
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-08-14 14:43:09 -0400

    Time: 46 ms (55.55%), Space: 16.4 MB (33.33%) - LeetHub

---
+++ 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countLetters(self, s: str) -> int:
+        total = 0
+        right, left = 0, 0
+        
+        while right < len(s):
+            while right < len(s) and s[right] == s[left]:
+                right += 1
+            subStr = right - left
+            # if subStr > 0:
+            total += (1 + subStr) * subStr // 2
+            left = right
+        return total
