935b5555b37515a89c289272fa1cc0d13a61ed61
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-08-14 15:45:37 -0400

    Time: 117 ms (89.91%), Space: 29.1 MB (17.35%) - LeetHub

--- 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
@@ -10,22 +10,17 @@ class Solution:
     def findRoot(self, tree: List['Node']) -> 'Node':
         if len(tree) == 1:
             return tree[0]
+        
         parents, children = set(), set()
         
         for node in tree:
             if node.children:
                 parents.add(node)
                 children.update(node.children)
-            
-        # print("parents =")
-        # for p in parents:
-        #     print(p.val, end=" ")
-        # print("children = ")
-        # for c in children:
-        #     print(c.val, end=" ")
         
         for p in parents:
             if p not in children:
                 return p
+            
         return None
         
