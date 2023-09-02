32e24cebdd0da9b6dbb4ff36de19c003b5b09b8f
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-09-02 14:48:40 -0400

    Time: 1945 ms (35.48%), Space: 17 MB (59.68%) - LeetHub

--- 0568-maximum-vacation-days/0568-maximum-vacation-days.py
+++ 0568-maximum-vacation-days/0568-maximum-vacation-days.py
@@ -1,49 +1,54 @@
 # Solution 1
 
-# class Solution:
+class Solution:
     
-#     def dfs(self, flights, days, curr_city, weekNo, n, k, memo):
-#         if weekNo == k:
-#             return 0
+    def dfs(self, flights, days, curr_city, weekNo, n, k, memo):
+        if weekNo == k:
+            return 0
         
-#         if memo[curr_city][weekNo]:
-#             return memo[curr_city][weekNo]
+        if memo[curr_city][weekNo]:
+            return memo[curr_city][weekNo]
         
-#         maxVacDay = 0
-#         for i in range(n):
-#             if i == curr_city or flights[curr_city][i] == 1:
-#                 vacationDays = days[i][weekNo] + self.dfs(flights, days, i, weekNo+1, n, k, memo)
-#                 maxVacDay = max(maxVacDay, vacationDays)
+        maxVacDay = 0
+        for i in range(n):
+            if i == curr_city or flights[curr_city][i] == 1:
+                vacationDays = days[i][weekNo] + self.dfs(flights, days, i, weekNo+1, n, k, memo)
+                maxVacDay = max(maxVacDay, vacationDays)
                 
-#         memo[curr_city][weekNo] = maxVacDay        
-#         return maxVacDay  
+        memo[curr_city][weekNo] = maxVacDay        
+        return maxVacDay  
         
-#     def maxVacationDays(self, flights: List[List[int]], days: List[List[int]]) -> int:
-#         n = len(flights)
-#         k = len(days[0])
+    def maxVacationDays(self, flights: List[List[int]], days: List[List[int]]) -> int:
+        n = len(flights)
+        k = len(days[0])
         
-#         memo = [[None] * k] * n 
+        memo = []
+        for i in range(n):
+            temp = [0 for _ in range(k)]
+            memo.append(temp)
         
-#         return self.dfs(flights, days, 0, 0, n, k, memo)
+        # memo = [[None] * k] * n 
+        
+        return self.dfs(flights, days, 0, 0, n, k, memo)
 
 
 # Solution 2
-class Solution:
-    def maxVacationDays(self, flights: List[List[int]], days: List[List[int]]) -> int:
-        cities = len(days)
-        weeks = len(days[0])
+# class Solution:
+#     def maxVacationDays(self, flights: List[List[int]], days: List[List[int]]) -> int:
+#         cities = len(days)
+#         weeks = len(days[0])
         
-        dp = []
-        for i in range(cities):
-            temp = [0 for _ in range(weeks+1)]
-            dp.append(temp)
+#         dp = []
+#         for i in range(cities):
+#             temp = [0 for _ in range(weeks+1)]
+#             dp.append(temp)
             
         
-        for  i in range(weeks-1, -1, -1):
-            for currCity in range(cities):
-                dp[currCity][i] = days[currCity][i] + dp[currCity][i+1]
-                for neighbourCity in range(cities):
-                    if flights[currCity][neighbourCity] == 1:
-                        dp[currCity][i] = max(days[neighbourCity][i] + dp[neighbourCity][i+1], dp[currCity][i])
+#         for  i in range(weeks-1, -1, -1):
+#             for currCity in range(cities):
+#                 dp[currCity][i] = days[currCity][i] + dp[currCity][i+1]
+#                 for neighbourCity in range(cities):
+#                     if flights[currCity][neighbourCity] == 1:
+#                         dp[currCity][i] = max(days[neighbourCity][i] + dp[neighbourCity][i+1], dp[currCity][i])
                         
-        return dp[0][0]
+#         return dp[0][0]
