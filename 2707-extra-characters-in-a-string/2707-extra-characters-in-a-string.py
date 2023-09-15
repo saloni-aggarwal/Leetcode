80a2eb2ca6bcf0f11124fb7196d78ae58c4b60c3
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-09-14 20:05:29 -0400

    Time: 321 ms (19.87%), Space: 16.3 MB (93.56%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,22 +1,39 @@
-class Solution:
-    def dp(self, start, n, s, memo, dictionary):
-        if start == n:
-            return 0
-        elif memo[start] != -1:
-            return memo[start]
+# # Solution 1 (top down approach)
+# class Solution:
+#     def dp(self, start, n, s, memo, dictionary):
+#         if start == n:
+#             return 0
+#         elif memo[start] != -1:
+#             return memo[start]
         
-        ans = self.dp(start+1, n, s, memo, dictionary) + 1
+#         ans = self.dp(start+1, n, s, memo, dictionary) + 1
         
-        for end in range(start, n):
-            subStr = s[start : end+1]
-            if subStr in dictionary:
-                ans = min(ans, self.dp(end+1, n, s, memo, dictionary))
+#         for end in range(start, n):
+#             subStr = s[start : end+1]
+#             if subStr in dictionary:
+#                 ans = min(ans, self.dp(end+1, n, s, memo, dictionary))
         
-        memo[start] = ans
-        return ans
+#         memo[start] = ans
+#         return ans
     
+#     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+#         n = len(s)
+#         memo = [-1] * n
+#         return self.dp(0, n, s, memo, dictionary)
+    
+
+# Solution 2
+class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         n = len(s)
-        memo = [-1] * n
-        return self.dp(0, n, s, memo, dictionary)
+        dp = [0] * (n+1)
         
+        for start in range(n-1, -1, -1):
+            dp[start] = dp[start+1] + 1
+            for end in range(start, n):
+                subStr = s[start:end+1]
+                if subStr in dictionary:
+                    dp[start] = min(dp[start], dp[end+1])
+        
+        return dp[0]
+                
