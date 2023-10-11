8753fd4a52d7a24811f83c48abf2150bf2498454
Saloni Aggarwal <89412654+saloni-aggarwal@users.noreply.github.com>
2023-10-10 21:06:09 -0400

    Time: 682 ms (94.28%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,6 +1,5 @@
-/* Write your T-SQL query statement below */
+# Write your MySQL query statement below
 SELECT A.lastName, A.firstName, B.city, B.state
 FROM Person A
 LEFT JOIN Address B
 ON A.personId = B.personId;
-
